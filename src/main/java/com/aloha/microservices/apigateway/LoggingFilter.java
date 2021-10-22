6fa4996624068c86d34e1743cd436e25173f66d7
mli <menghua.li@westjet.com>
2021-10-21 23:03:06 -0400

    add a global filter

---
+++ src/main/java/com/aloha/microservices/apigateway/LoggingFilter.java
@@ -0,0 +1,24 @@
+package com.aloha.microservices.apigateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+/**
+ * Sample of Global Fliter
+ */
+@Slf4j
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Path of the requst received -> {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+
+}
